61098519e71464e58d0ca59bda12081866ab6f20
ssettipalli <ssettipalli@ebsco.com>
2016-03-13 22:59:09 -0400

    added files

---
+++ Browser/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using System.Collections.ObjectModel;
+using System.Threading;
+
+namespace Browser
+{
+    public class BrowserDriver : IDisposable
+    {
+        internal IWebDriver MyDriver = null;
+        
+        /// <summary>
+        ///  constructor takes browser type
+        /// </summary>
+        /// <param name="thebrowsertype"></param>
+        public BrowserDriver(BrowserType thebrowsertype)
+        {
+            try
+            {
+
+
+                // Browsertype = thebrowsertype;
+                switch (thebrowsertype)
+                {
+                    case BrowserType.FireFox:
+                        MyDriver = new OpenQA.Selenium.Firefox.FirefoxDriver();
+                        break;
+                    case BrowserType.IE:
+
+                        MyDriver = new OpenQA.Selenium.IE.InternetExplorerDriver();
+
+                        break;
+                    case BrowserType.Chrome:
+                        MyDriver = new OpenQA.Selenium.Chrome.ChromeDriver();
+                        break;
+                }
+                MyDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(60));
+                MyDriver.Manage().Window.Maximize();
+            }
+             
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// <summary>
+        ///  Navigate
+        /// </summary>
+        /// <param name="theurl"></param>
+        public void Navigate(string theurl)
+        {
+            Console.WriteLine("Navigating to " + theurl);
+            MyDriver.Navigate().GoToUrl(theurl);
+
+        }
+
+        public enum BrowserType
+        {
+            FireFox = 1,
+            IE = 2,
+            Chrome = 3
+        }
+
+        /// <summary>
+        ///  Menu link
+        /// </summary>
+        /// <param name="theMenuLink"></param>
+        /// <returns></returns>
+        public IWebElement GetNavigationLink(string theMenuLink)
+        {
+            IWebElement aElement = MyDriver.FindElement(By.ClassName("navigation"));
+            ReadOnlyCollection<IWebElement> aCollectionElements = aElement.FindElements(By.TagName("a"));
+            foreach (IWebElement aEle in aCollectionElements)
+            {
+                if (aEle.Text.ToLower() == theMenuLink.ToLower())
+                    return aEle;
+            }
+
+            throw new Exception("Navigation link is not found." + theMenuLink);
+        }
+
+        /// <summary>
+        ///  Selecting a dropdown value based on value. 
+        /// </summary>
+        /// <param name="theValue"></param>
+        public IWebElement DropDown(string theValue)
+        {
+            IWebElement aElement = MyDriver.FindElement(By.Id("selectContainerProduct"));
+            aElement.Click();
+            //   aElement = aElement.FindElement(By.TagName("select"));
+            ReadOnlyCollection<IWebElement> aCollectionWebElements = aElement.FindElements(By.ClassName("active-result"));
+            foreach (IWebElement aEle in aCollectionWebElements)
+                if (aEle.Text.ToLower() == theValue.ToLower())
+                    return aEle;
+
+            throw new Exception("Dropdown Item not found." + theValue);
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string GetUrl()
+        {
+            return MyDriver.Url.ToLower();
+        }
+
+        /// <summary>
+        ///  Dispose driver object
+        /// </summary>
+        public void Dispose()
+        {
+            MyDriver.Quit();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="theSubMenuName"></param>
+        public IWebElement SubNavaigationLink(string theSubMenuLink)
+        {
+
+            Thread.Sleep(1000);
+            IWebElement aElement = (MyDriver.FindElement(By.Id("subNav")));
+            ReadOnlyCollection<IWebElement> aCollectionElements = aElement.FindElements(By.TagName("a"));
+            foreach (IWebElement aEle in aCollectionElements)
+            {
+                if (aEle.Text.ToLower() == theSubMenuLink.ToLower())
+                    return aEle;
+            }
+
+            throw new Exception("Sub Menu Navigation link is not found." + theSubMenuLink);
+
+        }
+
+        /// <summary>
+        ///  Get Executive MEember link.
+        /// </summary>
+        /// <param name="theExecutiveName"></param>
+        /// <returns></returns>
+        public IWebElement GetExecutiveMemberLink(string theExecutiveName)
+        {
+            IWebElement aElement;
+            string aHerfValue;
+            ReadOnlyCollection<IWebElement> aCollectionElements =
+                MyDriver.FindElements(By.ClassName("SecondaryCallToAction"));
+            foreach (IWebElement aEle in aCollectionElements)
+            {
+                aElement = aEle.FindElement(By.TagName("a"));
+
+                if (aElement.GetAttribute("href").ToLower().Contains(theExecutiveName.ToLower()))
+                    return aElement;
+            }
+
+            throw new Exception("Executive link is not found." + theExecutiveName);
+
+        }
+
+        /// <summary>
+        ///  Get ModalDialog source text
+        /// </summary>
+        /// <param name="theiframecount"></param>
+        /// <returns></returns>
+        public string GetModalDialogText(int theiframecount = 0)
+        {
+            Thread.Sleep(100);
+           return MyDriver.SwitchTo().Frame(theiframecount).PageSource;
+            throw new Exception("Modal Dialog is missing.");
+        }
+    }
+}
---
+++ Browser/TestNunit.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Browser;
+using System.Threading;
+
+namespace TestBrowser
+{
+    internal class TestNunit
+    {
+        protected BrowserDriver aBrowserDriver = null;
+        private string URL = "http://www.omgeo.com/";
+
+        [TestFixtureSetUp]
+        public void TestFixSetup()
+        {
+            aBrowserDriver = new BrowserDriver(Browser.BrowserDriver.BrowserType.FireFox);
+            aBrowserDriver.Navigate(URL);
+            Assert.AreEqual(aBrowserDriver.GetUrl(), URL, "Not Navigating to " + URL);
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixTearDown()
+        {
+            aBrowserDriver.Dispose();
+        }
+
+        [Test]
+        /// <summary>
+        /// Go to www.omgeo.com, click on “About” tab. verify that we navigate to the “about Omgeo” page
+        /// </summary>
+        public void Testcase1()
+        {
+
+            aBrowserDriver.GetNavigationLink("About").Click();
+            // verify navigated to about omgeo page
+            Assert.IsTrue(aBrowserDriver.GetUrl().Contains("http://www.omgeo.com/aboutomgeo"),
+                "not navigated to about page");
+        }
+
+        [Test]
+        public void Testcase2()
+        {
+            string aDropdownITem = "Alert";
+            Testcase1();
+            // click on Product Drop down and select ALERT option 
+            aBrowserDriver.DropDown(aDropdownITem).Click();
+            // verify alert option is selected
+            Assert.IsTrue(aBrowserDriver.GetUrl().Contains(URL + aDropdownITem.ToLower()),
+                "Not navigated to the Alert page");
+
+        }
+
+        [Test]
+        /// <summary>
+        /// Navigate to the “Leadership Team” page a. click on a member of the Executive Team b. verify a portion of the text is displayed specific to member.
+        /// </summary>
+        public void Testcase3()
+        {
+            string aSubMenuItem = "Leadership Team";
+            string aProfileName = "paula_Arthus";
+            string aLeadershipurl = "http://www.omgeo.com/leadership_team";
+
+            Testcase1();
+            aBrowserDriver.SubNavaigationLink(aSubMenuItem).Click();
+            aBrowserDriver.GetUrl().Contains(URL + aSubMenuItem);
+            Assert.IsTrue(aBrowserDriver.GetUrl().ToLower().Contains(aLeadershipurl),
+                "Not navigated to Leadership page.");
+            aBrowserDriver.GetExecutiveMemberLink(aProfileName).Click();
+            Thread.Sleep(1000);
+            // verify modal is opened up and opens up a correct modal.
+            string aModalTxt =aBrowserDriver.GetModalDialogText().ToLower();
+            Assert.IsTrue(
+                aModalTxt.Contains("bios/paula-arthus-omgeo.jpg"),
+                "Not navigated to corresponding profile page.");
+
+        }
+    }
+}
